70d34d8e2a01eb95a9afe7a4d7033934fc678ea9
Ferraz <121936547+ferraz888@users.noreply.github.com>
2024-02-11 13:24:21 +0100

    finition entrai joueur

--- katana/tourPlayeur.cpp
+++ katana/tourPlayeur.cpp
@@ -13,8 +13,19 @@ void attententRepPlayer(std::vector<Playeur> playeurs)
 	{
 		cout << "Joueur " << i << " : " << playeurs[i].getName() << " est en train de jouer" << endl;
 		cout << "Entrez 1 pour continuer" << endl;
+		// afficher les cartes du joueur
+		cout << "L'id du joueur " << i + 1 << " est : " << playeurs[i].getId() << endl;
+		cout << "Deck du joueur " << i + 1 << ": ";
+		vector<Carte> deckplayeur = playeurs[i].getDeckPlayer();
+		for (size_t j = 0; j < deckplayeur.size(); ++j) {
+			cout << deckplayeur[j].getName() << " ";
+		}
+		cout << endl;
 		cin >> rep;
-		i++;
+		if (rep == 1)
+		{
+			i++;
+		}
 	}
 	attententRepPlayer(playeurs);
 
