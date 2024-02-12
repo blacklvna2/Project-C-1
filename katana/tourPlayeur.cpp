145166f718783b637730db6f0a709e0dcae929fd
Ferraz <121936547+ferraz888@users.noreply.github.com>
2024-02-12 18:32:45 +0100

    chantose 2

--- katana/tourPlayeur.cpp
+++ katana/tourPlayeur.cpp
@@ -11,14 +11,10 @@ using namespace std;
 
 void attententRepPlayer(vector<Playeur> playeurs, Jeu game)
 {
-    int rep;
     int i = 0;
     while (i < game.getnbplayer()) // Change condition from <= to <
     {
         cout << "Joueur " << i + 1 << " : " << playeurs[i].getName() << " est en train de jouer" << endl;
-        // afficher les cartes du joueur
-        cout << "L'id du joueur " << i + 1 << " est : " << playeurs[i].getId() << endl;
-        cout << "Deck du joueur " << i + 1 << ": ";
         vector<Carte> deckplayeur = playeurs[i].getDeckPlayer();
         for (size_t j = 0; j < deckplayeur.size(); ++j) {
             cout << deckplayeur[j].getName() << " ";
@@ -33,38 +29,32 @@ void attententRepPlayer(vector<Playeur> playeurs, Jeu game)
         }
         int piocher = static_cast<int>(deckplayeur.size()) + 1; // Use static_cast to convert size_t to int
         cout << "pour piocher une carte entrer " << piocher << endl;
-
+        
         int rep = EntreNombre();
-
-
-
-        // si le joueur a jouer une carte
-        if (rep >= 1 && rep <= static_cast<int>(deckplayeur.size())) // Use static_cast to convert size_t to int
+        if (rep ==  piocher) 
         {
-            if (rep == 1) {
-                rep -= 1;
-            }
-            cout << "le joueur " << playeurs[i].getName() << " a jouer la carte " << deckplayeur[rep].getName() << endl;
-            // jouer la carte
-
-            /*
-                todo: effectuer l'action de la carte
-            */
-
-            // suprimer la carte du deck
-            deckplayeur.erase(deckplayeur.begin() + rep);
-            // mettre a jour le deck du joueur
-            playeurs[i].setDeckPlayer(deckplayeur);
+            piocherCarte(game.getdeck(), playeurs[i], game);
+            cout << "Le joueur " << playeurs[i].getName() << " a piocher une carte" << endl;
         }
-        else
-        {
-            //piocherCarte(deck, playeurs[i], game);
-            if (rep == deckplayeur.size() + 1) // Use static_cast to convert size_t to int
+        else {
+        // si le joueur a jouer une carte
+            if (rep >= 1 && rep <= deckplayeur.size())
             {
-                piocherCarte(game.getdeck(), playeurs[i], game);
+                cout << "le joueur " << playeurs[i].getName() << " a jouer la carte " << deckplayeur[rep - 1].getName() << endl;
+                // jouer la carte
+                /*
+                    todo: effectuer l'action de la carte
+                */
+                // suprimer la carte du deck
+                deckplayeur.erase(deckplayeur.begin() + rep - 1); // Subtract 1 from rep to access the correct index
+                // mettre a jour le deck du joueur
+                playeurs[i].setDeckPlayer(deckplayeur);
             }
-
-            cout << "Invalid input. Please enter a number between 1 and " << deckplayeur.size() + 1  << endl;
+            else
+            {
+                //piocherCarte(deck, playeurs[i], game);
+                cout << "Invalid input. Please enter a number between 1 and " << deckplayeur.size() + 1  << endl;
+            }
         }
         i++;
     }
@@ -72,13 +62,13 @@ void attententRepPlayer(vector<Playeur> playeurs, Jeu game)
 
 void piocherCarte(std::vector<Carte> deck, Playeur playeur, Jeu game)
 {
-	// piocher une carte
-	// fais la finction ajouter la carte au deck du joueur
-	playeur.getDeckPlayer().push_back(deck[0]);
-	// afficher la carte piocher
-	cout << "Le joueur " << playeur.getName() << " a piocher la carte " << deck[0].getName() << endl;
-	// suprimer la carte du deck
-	deck.erase(deck.begin());
-	// mettre a jour le deck du jeu
-	game.setdeck(deck);
+    // piocher une carte
+    // fais la finction ajouter la carte au deck du joueur
+    playeur.getDeckPlayer().push_back(deck[0]);
+    // afficher la carte piocher
+    cout << "Le joueur " << playeur.getName() << " a piocher la carte " << deck[0].getName() << endl;
+    // suprimer la carte du deck
+    deck.erase(deck.begin());
+    // mettre a jour le deck du jeu
+    game.setdeck(deck);
 }
