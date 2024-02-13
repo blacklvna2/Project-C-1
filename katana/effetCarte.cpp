d227e1a44a495df564a54e80ba81e5a242a75026
Evan Dévigne <eljdev12@outlook.fr>
2024-02-13 09:37:37 +0100

    include biblio in effetCarte.cpp

---
+++ katana/effetCarte.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <windows.h>
+#include "effetCarte.h"
+#include "Carte.h"
+#include "CAction.h"
+#include "CArme.h"
+#include "CPerma.h"
+#include "DescPerso.h"
+#include "distancePlayeur.h"
+#include "jeu.h"
+#include "playeur.h"
+#include "effetCarte.h"
+#include "Fonction.h"
+#include "initgame.h"
+#include "perso.h"
+#include "resource.h"
+#include "role.h"
+#include "tourPlayeur.h"
+
+int effetAction(std::string NameCarte) {
+
+}
