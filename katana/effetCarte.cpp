4120d746f5870ff6f370d121c8b4f1a3e41feeaf
Evan Dévigne <eljdev12@outlook.fr>
2024-02-13 12:49:49 +0100

    correction erreur + modification effetCarte

--- katana/effetCarte.cpp
+++ katana/effetCarte.cpp
@@ -4,61 +4,58 @@
 using namespace std;
 
 
-void effetAction(Carte carte, Playeur cible)
+void effetAction(Carte carte, Playeur cible, Playeur playeur)
 {
 
 }
 
-void effectCartPerma(Carte carte, Playeur* playeur)
+void effectCartPerma(Carte carte, Playeur playeur, Playeur cible)
 {
 }
 
-void effectCartePerso(Playeur Playeur, Carte carte)
+void effectCartePerso(Playeur playeur, Carte carte, Playeur cible)
 {
 }
 
 void effectCartArme(CArme carte, Playeur playeur, Playeur cible, int distance) {
-    if (carte.getPrecision() >= distance && playeur.getStatus() == "up")
-        {
+	if (carte.getPrecision() >= distance && playeur.getStatus() == "up") {
 		vector<Carte> tableaucarte = cible.getDeckPlayer();
 		Perso persoCible = cible.getPerso();
-            for (int i = 0; i<=tableaucarte.size(); i++)
-			{
-				if (tableaucarte[i].getName() == "Parade")
-				{
-					cout << "Voulez vous parer l'attaque ? 1: oui 2: non ";
-					int rep = EntreNombre();
-					cout << endl;
-					if (rep == 1)
-					{
-						cible.getDeckPlayer().erase(cible.getDeckPlayer().begin() + i);
-						cout << "Attaque parée" << endl;
-					}
-					else if (rep == 2)
-					{
-						persoCible.setPv(persoCible.getPv() - carte.getDegat());
-						cout << "Attaque réussie" << endl;
-						for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
-							if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
-								playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
-							}
+		for (int i = 0; i <= tableaucarte.size(); i++) {
+			if (tableaucarte[i].getName() == "Parade") {
+				cout << cible.getName() << ", voulez vous parer l'attaque ? 1: oui 2: non ";
+				int rep = EntreNombre();
+				cout << endl;
+				if (rep == 1) {
+					cible.getDeckPlayer().erase(cible.getDeckPlayer().begin() + i);
+					cout << "Attaque parée" << endl;
+					break;
+				}
+				else if (rep == 2) {
+					persoCible.setPv(persoCible.getPv() - carte.getDegat());
+					cout << "Attaque réussie" << endl;
+					for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
+						if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
+							playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
+							break;
 						}
 					}
-				else
-				{
-						persoCible.setPv(persoCible.getPv() - carte.getDegat());
-						cout << "Attaque réussie" << endl;
-						for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
-							if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
-								playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
-							}
-						}
 				}
 			}
-        else
-        {
-            cout << "Attaque loupée" << endl;
-        }
-    }
+			else {
+				persoCible.setPv(persoCible.getPv() - carte.getDegat());
+				cout << "Attaque réussie" << endl;
+				for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
+					if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
+						playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
+						break;
+					}
+				}
+			}
+		}
+	}
+	else {
+		cout << "Attaque loupée" << endl;
+	}
 }
 
