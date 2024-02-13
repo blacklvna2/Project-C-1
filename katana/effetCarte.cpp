c4bfc3171d5f8e2877e95d4cf718c43a742b9b41
Evan Dévigne <eljdev12@outlook.fr>
2024-02-13 11:31:55 +0100

    fonction effetArme

--- katana/effetCarte.cpp
+++ katana/effetCarte.cpp
@@ -1,5 +1,8 @@
-
 #include "effetCarte.h"
+#include <vector>
+
+using namespace std;
+
 
 void effetAction(Carte carte, Playeur cible)
 {
@@ -14,7 +17,48 @@ void effectCartePerso(Playeur Playeur, Carte carte)
 {
 }
 
-void effectCartArme(Carte carte, Playeur* playeur, Playeur* cible)
-{
+void effectCartArme(CArme carte, Playeur playeur, Playeur cible, int distance) {
+    if (carte.getPrecision() >= distance && playeur.getStatus())
+        {
+		vector<Carte> tableaucarte = cible.getDeckPlayer();
+		Perso persoCible = cible.getPerso();
+            for (int i = 0; i<=tableaucarte.size(); i++)
+			{
+				if (tableaucarte[i].getName() == "Parade")
+				{
+					cout << "Voulez vous parer l'attaque ? 1: oui 2: non ";
+					int rep = EntreNombre();
+					cout << endl;
+					if (rep == 1)
+					{
+						cible.getDeckPlayer().erase(cible.getDeckPlayer().begin() + i);
+						cout << "Attaque parée" << endl;
+					}
+					else if (rep == 2)
+					{
+						persoCible.setPv(persoCible.getPv() - carte.getDegat());
+						cout << "Attaque réussie" << endl;
+						for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
+							if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
+								playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
+							}
+						}
+					}
+				else
+				{
+						persoCible.setPv(persoCible.getPv() - carte.getDegat());
+						cout << "Attaque réussie" << endl;
+						for (int i = 0; i <= playeur.getDeckPlayer().size(); i++) {
+							if (playeur.getDeckPlayer()[i].getName() == carte.getName()) {
+								playeur.getDeckPlayer().erase(playeur.getDeckPlayer().begin() + i);
+							}
+						}
+				}
+			}
+        else
+        {
+            cout << "Attaque loupée" << endl;
+        }
+    }
 }
 
